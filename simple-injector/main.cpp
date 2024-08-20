ee8fdbf01e74a4bd296de6f3892a012afdabdd1e
Daniluk2 <59769523+Daniluk2@users.noreply.github.com>
2024-08-20 21:54:44 +0500

    refactored code
    1. fixed a memory leak
    2. refactored code: added user input and improved structure.

--- simple-injector/main.cpp
+++ simple-injector/main.cpp
@@ -2,16 +2,15 @@
 #include <windows.h>
 #include <psapi.h>
 
-#define DLL_NAME L"YOUR_DLL.dll"
-#define PROC_NAME L"YOUR_PROCESS.exe"
+wchar_t proc_name[MAX_PATH];
+wchar_t dll_patch[MAX_PATH];
 
-int main()
-{
+DWORD get_process_pid() {
   HANDLE h_heap = GetProcessHeap();
   DWORD process_count = 1024;
   DWORD* process_list = new DWORD[process_count];
   DWORD cb_alloc = process_count * sizeof(DWORD);
-  DWORD cbReturned;
+  DWORD cb_returned;
   DWORD targ_pid = 0;
 
   if (!process_list) {
@@ -20,8 +19,7 @@ int main()
     return false;
   }
 
-
-  if (!K32EnumProcesses(process_list, cb_alloc, &cbReturned)) {
+  if (!K32EnumProcesses(process_list, cb_alloc, &cb_returned)) {
     std::wcout << L"error K32EnumProcesses!\n";
     delete[] process_list;
     return false;
@@ -33,7 +31,7 @@ int main()
     if (h_process) {
       WCHAR process_name[MAX_PATH];
       if (K32GetModuleBaseNameW(h_process, nullptr, process_name, MAX_PATH)) {
-        if (wcscmp(process_name, PROC_NAME) == 0) {
+        if (wcscmp(process_name, proc_name) == 0) {
           std::wcout << L"Found process PID: " << pid << std::endl;
           targ_pid = pid;
           CloseHandle(h_process);
@@ -43,7 +41,6 @@ int main()
       CloseHandle(h_process);
     }
   }
-
   delete[] process_list;
 
   if (targ_pid == 0) {
@@ -51,12 +48,16 @@ int main()
     return false;
   }
 
+  return targ_pid;
+}
+
+bool inject_to_process(DWORD targ_pid) {
   HANDLE h_targproc = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_CREATE_THREAD, FALSE, targ_pid);
 
   DWORD len = 1024;
   LPWSTR buf = new WCHAR[len];
   LPWSTR part = nullptr;
-  if (GetFullPathNameW(DLL_NAME, len, buf, &part) == 0) {
+  if (GetFullPathNameW(dll_patch, len, buf, &part) == 0) {
     std::wcout << L"failed to get full path name!\n";
     delete[] buf;
     return false;
@@ -87,12 +88,30 @@ int main()
   }
 
   WaitForSingleObject(h_theard, INFINITE);
-
   CloseHandle(h_theard);
   VirtualFreeEx(h_targproc, alloc_mem, 0, MEM_RELEASE);
   CloseHandle(h_targproc);
+  delete[] buf;
 
-  std::wcout << L"dll injected!\n";
+  return true;
+}
 
+int main()
+{
+  std::wcout << L"enter the name of the process: ";
+  std::wcin.getline(proc_name, MAX_PATH);
+
+  std::wcout << L"enter the name of the DLL: ";
+  std::wcin.getline(dll_patch, MAX_PATH);
+
+  DWORD targ_pid = get_process_pid();
+  if (targ_pid != 0) {
+    if (inject_to_process(targ_pid))
+      std::wcout << L"dll injected!\n";
+    else
+      std::wcout << L"injection failed!\n";
+  }else
+    std::wcout << L"process not found or injection failed!\n";
+  
   return true;
 }
